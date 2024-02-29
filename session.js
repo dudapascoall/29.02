4571e5dfd940a04b4427f6745ae3401e70f899f4
dudapascoall <142435751+dudapascoall@users.noreply.github.com>
2024-02-29 12:00:49 -0300

    Add files via upload

---
+++ session.js
@@ -0,0 +1,15 @@
+sessionStorage.clear;
+
+function enviarSessao() {
+    let txt_login = document.getElementById("txt_login");
+    let div_sessoes = document.getElementById("div_sessoes");
+
+    let sessao_digitada = sessionStorage.getItem(txt_login.value);
+    if (sessao_digitada == null){
+        sessionStorage.getItem(txt_login.value, txt_login.value);
+        console.log("A sessao" + txt_login.value + "foi logada com sucesso.");
+        div_sessoes.innerHTML += "<br /> \r\n" + txt_login.value;
+    } else {
+        console.error("Nao foi possivel enviar login" + txt_login.value + ", pois ja ha uma sessao logada com esse nome");
+    }
+}
